f4144bb20720ab3e66ba243d3f8e6d3b1c577a66
Vincent Alexander <118685091+Alexander-2912@users.noreply.github.com>
2023-04-08 01:50:52 +0700

    Add files via upload

---
+++ Queries/Analytical Query.sql
@@ -0,0 +1,76 @@
+--Analytical Query
+--No 1
+SELECT *
+FROM car_product
+RIGHT JOIN bid
+  ON car_product.product_id = bid.product_id
+  
+CREATE MATERIALIZED VIEW jumlah_model
+AS
+SELECT model, COUNT(model) as count_product
+FROM car_product
+GROUP BY model;
+
+SELECT * 
+FROM jumlah_model
+
+SELECT car_product.model, count_product, COUNT(bid.product_id) as count_bid
+FROM car_product
+LEFT JOIN bid
+  ON car_product.product_id = bid.product_id
+LEFT JOIN jumlah_model
+	ON car_product.model = jumlah_model.model
+GROUP BY car_product.model, count_product
+ORDER BY count_bid DESC;
+
+--No 2
+SELECT nama_kota, brand, model, year, price, AVG(price) OVER (PARTITION BY nama_kota) as avg_car_city
+FROM car_product
+JOIN seller
+  ON seller.product_id = car_product.product_id
+JOIN location
+  ON seller.kota_id = location.kota_id
+GROUP BY nama_kota, brand, model,year, price
+ORDER BY avg_car_city ASC;
+
+--No 3
+SELECT 	model, 
+		user_id, 
+		bid_date as first_bid_date,
+		LEAD(bid_date,1) OVER(PARTITION BY bid_date ORDER BY bid_date) as next_bid_date,
+		bid.price as first_bid_price,
+		LEAD(bid.price,1) OVER(PARTITION BY bid_date ORDER BY bid.price) as next_bid_price
+FROM bid
+JOIN car_product
+	ON bid.product_id = car_product.product_id
+WHERE model = 'Toyota Yaris'
+ORDER BY user_id;
+
+--No 4
+SELECT car_product.model, AVG(car_product.price) as avg_price
+FROM car_product
+GROUP BY model;
+
+SELECT	car_product.model,
+		AVG(car_product.price) as avg_price,
+		AVG(bid_price) as avg_bid_6month
+FROM bid
+JOIN car_product
+  ON car_product.product_id = bid.product_id
+WHERE bid_date BETWEEN '2022-01-01' AND '2022-06-30'
+GROUP BY car_product.model;
+
+
+--No 5
+SELECT brand, model,
+  AVG(CASE WHEN EXTRACT(MONTH FROM bid_date) = 1 THEN bid_price END) AS month1_avg,
+  AVG(CASE WHEN EXTRACT(MONTH FROM bid_date) = 2 THEN bid_price END) AS month2_avg,
+  AVG(CASE WHEN EXTRACT(MONTH FROM bid_date) = 3 THEN bid_price END) AS month3_avg,
+  AVG(CASE WHEN EXTRACT(MONTH FROM bid_date) = 4 THEN bid_price END) AS month4_avg,
+  AVG(CASE WHEN EXTRACT(MONTH FROM bid_date) = 5 THEN bid_price END) AS month5_avg,
+  AVG(CASE WHEN EXTRACT(MONTH FROM bid_date) = 6 THEN bid_price END) AS month6_avg
+FROM bid
+JOIN car_product
+  ON car_product.product_id = bid.product_id
+WHERE bid_date BETWEEN '2022-01-01' AND '2022-06-30'  AND model = 'Toyota Yaris'
+GROUP BY car_product.brand, car_product.model;
---
+++ Queries/Transactional Query.sql
@@ -0,0 +1,64 @@
+
+--Transactional Query
+--No 1
+SELECT product_id, brand, model, year, price
+FROM car_product
+WHERE year >= 2015;
+
+--No 2
+INSERT INTO bid (bid_id, user_id, product_id, bid_date, price, bid_price, bid_status)
+VALUES (90, 1, 2, '2022-02-14', 215000000, 220000000, 'sent');
+
+--No 3
+SELECT seller.product_id, brand, model, year, price, date_post
+FROM seller
+INNER JOIN car_product
+ON seller.product_id = car_product.product_id
+WHERE seller.nama = 'Masha Branscomb';
+
+--No 4
+SELECT product_id, brand, model, year, price
+FROM car_product
+WHERE model LIKE '%Yaris'
+ORDER BY price ASC;
+
+--No 5
+CREATE FUNCTION public.distance(point1 POINT, point2 POINT)
+RETURNS float AS $$
+DECLARE
+	lon1 float := radians(point1[0]);
+	lat1 float := radians(point1[1]);
+	lon2 float := radians(point2[0]);
+	lat2 float := radians(point2[1]);
+  	
+	dlon float:= lon2 - lon1;
+  	dlat float:= lat2 - lat1;
+	a float;
+	c float;
+	r float := 6371;
+	jarak float;
+BEGIN
+	a:= sin(dlat/2)^2 + cos(lat1) * cos(lat2) * sin(dlon/2)^2;
+	c:= 22 * asin(sqrt(a));
+	jarak := r * c;
+	
+	RETURN jarak;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP FUNCTION distance;
+
+SELECT product_id, brand, model, year, price, distance((SELECT location.kordinat 
+													  FROM seller
+													  JOIN location 
+													  ON seller.kota_id = location.kota_id
+													  WHERE seller.kota_id = 3173 LIMIT 1
+													  ),
+													 (SELECT location.kordinat 
+													  FROM car_product
+													  JOIN location 
+													  ON car_product.kota_id = location.kota_id
+													  WHERE car_product.kota_id = 3173 LIMIT 1
+													  ))
+FROM car_product
+WHERE kota_id = 3173;
